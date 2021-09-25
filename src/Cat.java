5ec39de11e43feb11cc6b04643675f05d57ae649
Pantiukhov Roman <pantiukhov.r@gmail.com>
2021-09-25 15:18:40 +0300

    New Commit

---
+++ src/Cat.java
@@ -0,0 +1,6 @@
+public class Cat extends Animal {
+    @Override
+    public void move() {
+        System.out.println("Cat can move very slow and quietly");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.move();
+        Cat cat = new Cat();
+        cat.move();
+        SubClass s = new SubClass(10, 12);
+        System.out.println(s.add(10));
+    }
+}
+
+class SuperClass {
+    public int a;
+    public int b;
+
+    SuperClass(int a) {
+        this.a = 15;
+        this.b = 33;
+    }
+
+    public int add(int v) {
+        this.a += v;
+        return a;
+    }
+}
+class SubClass extends SuperClass {
+    SubClass(int a, int b) {
+        super(a);
+        this.b = b;
+    }
+
+    @Override
+    public int add(int v) {
+        return super.add(v) + a;
+    }
+
+
+}
